0edf7813795eb6245487493c91cb198b4baaf941
Yihan <yih.chen@outlook.com>
2023-07-13 11:56:22 +0800

    Code Updata

--- Other_Code/take_intersection.R
+++ Other_Code/take_intersection.R
@@ -1,25 +1,27 @@
 setwd('/data/yihan/Mxra7_2m/')
-cell_types = c("B cells",
+cell_types = c("B_cells",
                "Erythrocytes",
                "Granulocytes",
                "Monocytes",
-               "T cells")
+               "T_cells")
 
 SingleR_Subset <- function(cell_type){
-  file1 = read.table(paste0("./Result_for_WT/05.edgeRMarkgenes.Result/", gsub(" ", "_", cell_type), ".control_treat.glmLRT.up.txt"))
-  genes1 = rownames(file1)
-  file2 = read.table(paste0("./Result_for_X/05.edgeRMarkgenes.Result/", gsub(" ", "_", cell_type), ".control_treat.glmLRT.up.txt"))
-  genes2 = rownames(file2)
-  intersection = intersect(genes1, genes2)
-  writeLines(intersection, paste0("./Result_for_combine/", gsub(" ", "_", cell_type), ".up.txt"))
+  markersX = read.delim(paste0("./Result_for_X/05.edgeRMarkgenes.Result/", cell_type, ".control_treat.glmLRT.plot.txt"), row.names = 1, sep = '\t', check.names = FALSE)
+  gene_upX = markers[which(markersX$sig=='up'),]
+  gene_up_nameX = rownames(gene_upX)
+  markersWT = read.delim(paste0("./Result_for_WT/05.edgeRMarkgenes.Result/", cell_type, ".control_treat.glmLRT.plot.txt"), row.names = 1, sep = '\t', check.names = FALSE)
+  gene_upWT = markers[which(markersWT$sig=='up'),]
+  gene_up_nameWT = rownames(gene_upWT)
+  intersectionUP = intersect(gene_up_nameX, gene_up_nameWT)
+  writeLines(intersectionUP, paste0("./Result_for_interstection/", gsub(" ", "_", cell_type), ".up.txt"))
   
-  file3 = read.table(paste0("./Result_for_WT/05.edgeRMarkgenes.Result/", gsub(" ", "_", cell_type), ".control_treat.glmLRT.down.txt"))
-  genes3 = rownames(file3)
-  file4 = read.table(paste0("./Result_for_X/05.edgeRMarkgenes.Result/", gsub(" ", "_", cell_type), ".control_treat.glmLRT.down.txt"))
-  genes4 = rownames(file4)
-  intersection = intersect(genes3, genes4)
-  writeLines(intersection, paste0("./Result_for_combine/", gsub(" ", "_", cell_type), ".down.txt"))}
-
+  gene_downX = markers[which(markersX$sig=='down'),]
+  gene_down_nameX = rownames(gene_downX)
+  gene_downWT = markers[which(markersWT$sig=='down'),]
+  gene_down_nameWT = rownames(gene_downWT)
+  intersectionDOWN = intersect(gene_down_nameX, gene_down_nameWT)
+  writeLines(intersectionDOWN, paste0("./Result_for_interstection/", gsub(" ", "_", cell_type), ".down.txt"))}
+  
 for (cell_type in cell_types){
   SingleR_Subset(cell_type)
 }
