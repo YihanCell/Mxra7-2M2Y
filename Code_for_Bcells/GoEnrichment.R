2df34225dafdb8363ea7d5647bc4c16909a2487d
Yihan <yih.chen@outlook.com>
2023-07-28 20:23:59 +0800

    Update Code

---
+++ Code_for_Bcells/GoEnrichment.R
@@ -0,0 +1,86 @@
+library(org.Mm.eg.db)
+library(R.utils)
+library(MAST)
+library(clusterProfiler)
+library(ggrepel)
+library(svglite)
+library(ggplot2)
+
+setwd('/data/yihan/Mxra7_2m/')
+
+R.utils::setOption("clusterProfiler.download.method",'auto')
+KEGG_database = 'mmu'
+
+markers = read.delim("/data/yihan/Mxra7_2m/05.edgeRMarkgenes.Result/B_cells.control_treat.glmLRT.plot.txt", row.names = 1, sep = '\t', check.names = FALSE)
+gene_all = markers[which(markers$sig %in% c('down', 'up')), ]
+gene_all = rownames(gene_all)
+gene_all = as.character(na.omit(AnnotationDbi::select(org.Mm.eg.db,
+                                                       keys = gene_all,
+                                                       columns = 'ENTREZID',
+                                                       keytype = 'SYMBOL')[,2]))
+geneset = gene_all
+ego_CC = enrichGO(gene = geneset,
+                    OrgDb=org.Mm.eg.db,
+                    keyType = "ENTREZID",
+                    ont = "CC",
+                    pAdjustMethod = "BH",
+                    minGSSize = 1,
+                    pvalueCutoff = 0.05,
+                    qvalueCutoff = 0.05,
+                    readable = TRUE)
+ego_BP = enrichGO(gene = geneset,
+                    OrgDb=org.Mm.eg.db,
+                    keyType = "ENTREZID",
+                    ont = "BP",
+                    pAdjustMethod = "BH",
+                    minGSSize = 1,
+                    pvalueCutoff = 0.05,
+                    qvalueCutoff = 0.05,
+                    readable = TRUE)
+ego_MF = enrichGO(gene = geneset,
+                    OrgDb=org.Mm.eg.db,
+                    keyType = "ENTREZID",
+                    ont = "MF",
+                    pAdjustMethod = "BH",
+                    minGSSize = 1,
+                    pvalueCutoff = 0.05,
+                    qvalueCutoff = 0.05,
+                    readable = TRUE)
+dev.new()
+dotplot(ego_BP)
+dev.off()
+ggsave(paste0("./Result_for_X/05.edgeRMarkgenes.Result/Enrich_GO/", cell_type, ".down_gene.Go_BP.pdf"), plot = BP.Go.up)
+  
+display_number = c(10,10,10)
+ego_result_BP = as.data.frame(ego_BP)[1:display_number[1], ]
+ego_result_CC = as.data.frame(ego_CC)[1:display_number[2], ] 
+ego_result_MF = as.data.frame(ego_MF)[1:display_number[3], ]
+  
+go_enrich_df = data.frame(
+  ID=c(ego_result_BP$ID, ego_result_CC$ID, ego_result_MF$ID), Description=c(ego_result_BP$Description,ego_result_CC$Description,ego_result_MF$Description),
+  GeneNumber=c(ego_result_BP$Count, ego_result_CC$Count, ego_result_MF$Count),
+  type=factor(c(rep("biological process", display_number[1]), 
+                  rep("cellular component", display_number[2]),
+                  rep("molecular function", display_number[3])), 
+                levels=c("biological process", "cellular component","molecular function" )))
+  
+for(i in 1:nrow(go_enrich_df)){
+    description_splite=strsplit(go_enrich_df$Description[i],split = " ")
+    description_collapse=paste(description_splite[[1]][1:10],collapse = " ") 
+    go_enrich_df$Description[i]=description_collapse
+    go_enrich_df$Description=gsub(pattern = "NA","",go_enrich_df$Description)
+  }
+  
+go_enrich_df$type_order=factor(rev(as.integer(rownames(go_enrich_df))),labels=rev(go_enrich_df$Description))
+COLS = c("#66C3A5", "#8DA1CB", "#FD8D62")
+p.Go.up = ggplot(data=go_enrich_df, aes(x=type_order,y=GeneNumber, fill=type)) + 
+    geom_bar(stat="identity", width=0.8) + 
+    scale_fill_manual(values = COLS) + 
+    coord_flip() + 
+    xlab("GO term") + 
+    ylab("Gene_Number") + 
+    labs(title = paste0("The Most Enriched GO Terms ",markers.screen_type))+
+    theme_bw() + theme(axis.text.y=element_text(size=8, angle=45, hjust=1))
+ggsave(paste0("./Result_for_X/05.edgeRMarkgenes.Result/Enrich_GO/", cell_type, ".down_gene.Go.pdf"), plot = p.Go.up)
+
+
--- Code_for_Monocytes/KEGG.R
+++ Code_for_Monocytes/KEGG.R
@@ -10,7 +10,7 @@ setwd('/data/yihan/Mxra7_2m/')
 R.utils::setOption("clusterProfiler.download.method",'auto')  
 KEGG_database = 'mmu'
 
-markers = read.delim("/data/yihan/Mxra7_2m/05.edgeRMarkgenes.Result/B_cells.control_treat.glmLRT.plot.txt", row.names = 1, sep = '\t', check.names = FALSE)
+markers = read.delim("/data/yihan/Mxra7_2Y/05.edgeRMarkgenes.Result/Monocytes.control_treat.glmLRT.plot.txt", row.names = 1, sep = '\t', check.names = FALSE)
 
 gene_all = markers[which(markers$sig %in% c('down', 'up')), ]
 gene_all = rownames(gene_all)
@@ -20,10 +20,10 @@ gene_all = as.character(na.omit(AnnotationDbi::select(org.Mm.eg.db,
                                                       columns = 'ENTREZID',
                                                       keytype = 'SYMBOL')[,2]))
 geneset = gene_all
-  
+
 kegg.histogram.down = as.data.frame(enrichKEGG(gene = geneset,organism= KEGG_database, qvalueCutoff = 1, pvalueCutoff= 1))
 write.csv(kegg.histogram.down, "/data/yihan/Mxra7_2m/Result_for_Bcells/2m_all_gene_KEGG.csv")
-  
+
 rownames(kegg.histogram.down) = 1:nrow(kegg.histogram.down)
 kegg.histogram.down$order=factor(rev(as.integer(rownames(kegg.histogram.down))),labels = rev(kegg.histogram.down$Description))
 kegg.histogram.down$Description = gsub(' - Mus musculus \\(house mouse\\)','',kegg.histogram.down$Description)
@@ -40,8 +40,8 @@ kegg.plot.down = ggplot(kegg.histogram.down,aes(y=reorder(order,Count),x=Count,f
   geom_bar(stat = "identity",width=0.7)+
   scale_fill_gradient(low = "red",high ="blue" )+
   labs(title = "B cells KEGG Pathways Enrichment",
-        x = "Gene numbers", 
-        y = "Pathways")+
+       x = "Gene numbers", 
+       y = "Pathways")+
   theme(axis.title.x = element_text(face = "bold",size = 8),
         axis.title.y = element_text(face = "bold",size = 8),
         legend.title = element_text(face = "bold",size = 8),
