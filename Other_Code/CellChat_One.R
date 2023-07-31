922f21e335a2aee3887ce22f2274ed9fc30510e0
Yihan <yih.chen@outlook.com>
2023-08-01 00:49:52 +0800

    Update Code

--- Other_Code/CellChat_One.R
+++ Other_Code/CellChat_One.R
@@ -21,6 +21,16 @@ meta1 = data.frame(group = labels, row.names = names(labels))
 cellchat.wt2y = addMeta(cellchat.wt2y, meta = meta1, meta.name = "labels")
 cellchat.wt2y = setIdent(cellchat.wt2y, ident.use = "labels")
 cellchat.wt2y@DB = CellChatDB.use
+cellchat.wt2y = subsetData(cellchat.wt2y)
+cellchat.wt2y = identifyOverExpressedGenes(cellchat.wt2y)
+cellchat.wt2y = identifyOverExpressedInteractions(cellchat.wt2y)
+cellchat.wt2y = computeCommunProb(cellchat.wt2y)
+cellchat.wt2y = filterCommunication(cellchat.wt2y, min.cells = 10)
+cellchat.wt2y = computeCommunProbPathway(cellchat.wt2y)
+cellchat.wt2y = aggregateNet(cellchat.wt2y)
+groupSize.wt2y = as.numeric(table(cellchat.wt2y@idents))
+cellchat.wt2y = netAnalysis_computeCentrality(cellchat.wt2y, slot.name = "netP")
+
 
 ## ko2y
 labels = combined.2y.ko@meta.data$predicted_celltype
@@ -30,21 +40,48 @@ meta2 = data.frame(group = labels, row.names = names(labels))
 cellchat.ko2y = addMeta(cellchat.ko2y, meta = meta2, meta.name = "labels")
 cellchat.ko2y = setIdent(cellchat.ko2y, ident.use = "labels")
 cellchat.ko2y@DB = CellChatDB.use
+cellchat.ko2y = subsetData(cellchat.ko2y)
+cellchat.ko2y = identifyOverExpressedGenes(cellchat.ko2y)
+cellchat.ko2y = identifyOverExpressedInteractions(cellchat.ko2y)
+cellchat.ko2y = computeCommunProb(cellchat.ko2y)
+cellchat.ko2y = filterCommunication(cellchat.ko2y, min.cells = 10)
+cellchat.ko2y = computeCommunProbPathway(cellchat.ko2y)
+cellchat.ko2y = aggregateNet(cellchat.ko2y)
+groupSize.ko2y = as.numeric(table(cellchat.ko2y@idents))
+cellchat.ko2y = netAnalysis_computeCentrality(cellchat.ko2y, slot.name = "netP")
 
 ## wt2m
 labels = combined.2m.wt@meta.data$predicted_celltype
 data.input.wt2m = GetAssayData(combined.2m.wt, assay = "RNA", slot = "data") # normalized data matrix
 cellchat.wt2m = createCellChat(object = data.input.wt2m)
 meta3 = data.frame(group = labels, row.names = names(labels)) 
-cellchat.wt2m = addMeta(cellchat.wt2m, meta = meta1, meta.name = "labels")
+cellchat.wt2m = addMeta(cellchat.wt2m, meta = meta3, meta.name = "labels")
 cellchat.wt2m = setIdent(cellchat.wt2m, ident.use = "labels")
 cellchat.wt2m@DB = CellChatDB.use
+cellchat.wt2m = subsetData(cellchat.wt2m)
+cellchat.wt2m = identifyOverExpressedGenes(cellchat.wt2m)
+cellchat.wt2m = identifyOverExpressedInteractions(cellchat.wt2m)
+cellchat.wt2m = computeCommunProb(cellchat.wt2m)
+cellchat.wt2m = filterCommunication(cellchat.wt2m, min.cells = 10)
+cellchat.wt2m = computeCommunProbPathway(cellchat.wt2m)
+cellchat.wt2m = aggregateNet(cellchat.wt2m)
+groupSize.wt2m = as.numeric(table(cellchat.wt2m@idents))
+cellchat.wt2m = netAnalysis_computeCentrality(cellchat.wt2m, slot.name = "netP")
 
 ## ko2m
 labels = combined.2m.ko@meta.data$predicted_celltype
 data.input.ko2m = GetAssayData(combined.2m.ko, assay = "RNA", slot = "data") # normalized data matrix
 cellchat.ko2m = createCellChat(object = data.input.ko2m)
 meta4 = data.frame(group = labels, row.names = names(labels)) 
-cellchat.ko2m = addMeta(cellchat.ko2m, meta = meta1, meta.name = "labels")
+cellchat.ko2m = addMeta(cellchat.ko2m, meta = meta4, meta.name = "labels")
 cellchat.ko2m = setIdent(cellchat.ko2m, ident.use = "labels")
-cellchat.wt2m@DB = CellChatDB.use
+cellchat.ko2m@DB = CellChatDB.use
+cellchat.ko2m = subsetData(cellchat.ko2m)
+cellchat.ko2m = identifyOverExpressedGenes(cellchat.ko2m)
+cellchat.ko2m = identifyOverExpressedInteractions(cellchat.ko2m)
+cellchat.ko2m = computeCommunProb(cellchat.ko2m)
+cellchat.ko2m = filterCommunication(cellchat.ko2m, min.cells = 10)
+cellchat.ko2m = computeCommunProbPathway(cellchat.ko2m)
+cellchat.ko2m = aggregateNet(cellchat.ko2m)
+groupSize.ko2m = as.numeric(table(cellchat.ko2m@idents))
+cellchat.ko2m = netAnalysis_computeCentrality(cellchat.ko2m, slot.name = "netP")
---
+++ Other_Code/CellChat_Two.R
@@ -0,0 +1,46 @@
+library(CellChat)
+library(patchwork)
+
+object.list.all = list(wt2m = cellchat.wt2m, ko2m = cellchat.ko2m, wt2y = cellchat.wt2y, ko2y = cellchat.ko2y)
+cellchat.all = mergeCellChat(object.list, add.names = names(object.list), cell.prefix = TRUE)
+cellchat.all
+
+object.list.2m = list(wt2m = cellchat.wt2m, ko2m = cellchat.ko2m)
+cellchat.2m = mergeCellChat(object.list.2m, add.names = names(object.list.2m))
+cellchat.2m
+
+object.list.2y = list(wt2y = cellchat.wt2y, ko2y = cellchat.ko2y)
+cellchat.2y = mergeCellChat(object.list.2y, add.names = names(object.list.2y))
+cellchat.2y
+
+dev.new()
+gg1 = compareInteractions(cellchat.all, show.legend = F, group = c(1,2))
+gg2 = compareInteractions(cellchat.all, show.legend = F, group = c(1,2), measure = "weight")
+gg1 + gg2
+dev.off()
+
+dev.new()
+netVisual_heatmap(cellchat.2m)
+dev.off()
+dev.new()
+netVisual_heatmap(cellchat.2m, measure = "weight")
+dev.off()
+
+custom_variable_order <- c("Granulocytes", "T cells", "B cells", "Monocytes", "Erythrocytes")
+dev.new()
+netVisual_heatmap(cellchat.2y)
+dev.off()
+dev.new()
+netVisual_heatmap(cellchat.2y, measure = "weight")
+dev.off()
+
+dev.new()
+netVisual_heatmap(cellchat)
+dev.off()
+
+#===========
+dev.new() 
+gg1 <- rankNet(cellchat.2m, mode = "comparison", stacked = T, do.stat = TRUE)
+gg2 <- rankNet(cellchat.2m, mode = "comparison", stacked = F, do.stat = TRUE)
+gg1 + gg2
+dev.off()
